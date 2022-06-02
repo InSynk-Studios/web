e327a0432f11e7c1402aa89b4abdaa9ee4726bb7
Manish <manishsdlp222@gmail.com>
2022-06-02 20:42:59 +0530

    fix: removed extra background blob

--- components/UI/Background.js
+++ components/UI/Background.js
@@ -18,18 +18,11 @@ function Background(props) {
   return (
     <Fragment>
       <div
-        className={`absolute top-[30rem] left-[42rem] w-72 h-72 bg-pink-800 rounded-full filter blur-lg animation-delay-4000 ${
+        className={`absolute bottom-12 left-[80rem] w-[40rem] h-[40rem] bg-circle rounded-full filter blur-2xl opacity-75 animation-delay-1000 ${
           animateBackground && "animate-blob"
         }`}
       ></div>
-      <div
-        className={`absolute bottom-12 left-[75rem] w-[40rem] h-[40rem] bg-circle rounded-full filter blur-2xl opacity-75 animation-delay-1000 ${
-          animateBackground && "animate-blob"
-        }`}
-      ></div>
-      <section>
-          {props.children}
-      </section>
+      <section>{props.children}</section>
     </Fragment>
   );
 }
