b89487ebf0a30e1722b98c45f7efc9ca05130f22
Manish <manishsdlp222@gmail.com>
2022-06-06 23:19:31 +0530

    feat(client.js): dynamically rendering client list

--- components/Client.js
+++ components/Client.js
@@ -1,4 +1,11 @@
+import ClientRow from "./client/ClientRow";
+
 function Client() {
+  var row = [];
+  for (var i = 0; i < 3; i++) {
+    row.push(<ClientRow key={i} />);
+  }
+
   return (
     <section className="my-40 text-white">
       <div className="max-w-screen-xl mx-auto">
@@ -8,31 +15,9 @@ function Client() {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 mt-16 sm:grid-cols-2 lg:grid-cols-4">
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
-          <div className="flex flex-col items-center justify-between p-10 border border-gray-600">
-            <div className="w-32 h-10 rounded-2xl bg-neutral-800"></div>
-          </div>
+        <div className="grid grid-cols-2 mt-16 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="border-l border-gray-600">{row[0]}</div>
+          {row}
         </div>
       </div>
     </section>
--- components/UI/Background.js
+++ components/UI/Background.js
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 function Background(props) {
   return (
     <Fragment>
-      <main className="container max-w-7xl mx-auto px-8">{props.children}</main>
+      <main className="container max-w-7xl mx-auto">{props.children}</main>
     </Fragment>
   );
 }
