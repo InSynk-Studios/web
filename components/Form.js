49299915558f5ba4a4f911b72e28869605d4a0b7
Manish <manishsdlp222@gmail.com>
2022-08-30 11:42:07 +0530

    feat: add socials and contact details

--- components/Form.js
+++ components/Form.js
@@ -1,5 +1,9 @@
 import { Fragment, useRef, useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import linkedinsvg from "../assets/socials/linkedin.svg";
+import twittersvg from "../assets/socials/twitter.svg";
+import instagramsvg from "../assets/socials/instagram.svg";
+import Image from "next/image";
 
 const isEmpty = (value) => value.trim() === "";
 
@@ -76,91 +80,137 @@ function Form() {
     <Fragment>
       <section className="overflow-hidden relative font-inter-400" id="contact">
         <div className="container">
-          <div className="flex flex-col lg:flex-row lg:items-center text-gray-200 lg:justify-end">
-            <div className="w-full lg:w-2/3 xl:w-6/12">
-              <div className="relative lg:left-10 rounded-lg py-8 sm:p-12 shadow-lg">
-                <div className="max-w-xl mb-12 lg:mb-0">
-                  <h1 className="text-4xl md:text-6xl font-semibold text-white">
-                    Let&rsquo;s chat about your project
-                  </h1>
-                  <p className="text-lg md:text-xl pt-5 text-gray-400">
-                    You can reach us anytime at{" "}
-                    <span className="text-white">
-                      support@insynkstudios.com
-                    </span>
-                  </p>
+          <div className="flow-root">
+            <div className="float-left lg:absolute bottom-11 text-white text-xl">
+              Reach us out at :
+              <br />
+              <a
+                href="aman@insynkstudios.com"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline text-violet-500"
+              >
+                aman@insynkstudios.com
+              </a>
+              <br />
+              <a
+                href="vishnu@insynkstudios.com"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline text-violet-500"
+              >
+                vishnu@insynkstudios.com
+              </a>
+              <p className="py-6">
+                Call us at :
+                <br />
+                <span className="text-violet-500">+91 86965 65066</span>
+                <br />
+                <span className="text-violet-500">+91 8009342234</span>
+              </p>
+              <p>
+                Our socials :
+                <br />
+                <a
+                  href="https://www.linkedin.com/company/0xinsynkstudios"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={linkedinsvg} alt="linkedin" />
+                </a>
+                <a
+                  href="https://twitter.com/0xinsynkstudios"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={twittersvg} alt="twitter" />
+                </a>
+                <a
+                  href="https://www.instagram.com/0xinsynkstudios/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image src={instagramsvg} alt="instagram" />
+                </a>
+              </p>
+            </div>
+
+            <div className="float-right relative lg:left-10 rounded-lg py-8 sm:p-12 shadow-lg">
+              <div className="max-w-xl mb-12 lg:mb-0">
+                <h1 className="text-4xl md:text-6xl font-semibold text-white">
+                  Let&rsquo;s chat about your project
+                </h1>
+              </div>
+
+              <form role="form" onSubmit={confirmHandler}>
+                <input type="hidden" name="_captcha" value="false" />
+
+                <div
+                  className={`${"mb-6 mt-12"} ${
+                    formInputValidity.name ? "" : "border-red-500"
+                  }`}
+                >
+                  <div className="pb-3 text-white">
+                    <label htmlFor="name">Name</label>
+                  </div>
+                  <input type="text" name="_honey" className="hidden" />
+                  <input
+                    type="text"
+                    placeholder="Your Name"
+                    name="name"
+                    className={`${"w-full rounded-lg p-3 text-gray-50 bg-transparent border-2 border-grey-200 outline-none focus-visible:shadow-none focus:border-blue-200"}
+                      ${!formInputValidity.name && "border-red-500"}`}
+                    ref={nameInputRef}
+                    id="name"
+                  />
+                </div>
+                <div
+                  className={`${"mb-6"} ${
+                    formInputValidity.email ? "" : "border-red-500"
+                  }`}
+                >
+                  <div className="pb-3 text-white">
+                    <label htmlFor="address">Email</label>
+                  </div>
+                  <input
+                    type="email"
+                    placeholder="you@company.com"
+                    id="email"
+                    name="email"
+                    ref={emailInputRef}
+                    className={`${"w-full rounded-lg p-3 text-gray-50 bg-transparent border-2 border-grey-200 outline-none focus-visible:shadow-none focus:border-blue-200"}
+                      ${!formInputValidity.email && "border-red-500"}`}
+                    required
+                  />
                 </div>
 
-                <form role="form" onSubmit={confirmHandler}>
-                  <input type="hidden" name="_captcha" value="false" />
-
-                  <div
-                    className={`${"mb-6 mt-12"} ${
-                      formInputValidity.name ? "" : "border-red-500"
-                    }`}
-                  >
-                    <div className="pb-3 text-white">
-                      <label htmlFor="name">Name</label>
-                    </div>
-                    <input type="text" name="_honey" className="hidden" />
-                    <input
-                      type="text"
-                      placeholder="Your Name"
-                      name="name"
-                      className={`${"w-full rounded-lg p-3 text-gray-50 bg-transparent border-2 border-grey-200 outline-none focus-visible:shadow-none focus:border-blue-200"}
-                      ${!formInputValidity.name && "border-red-500"}`}
-                      ref={nameInputRef}
-                      id="name"
-                    />
+                <div
+                  className={`${"mb-6"} ${
+                    formInputValidity.message ? "" : "border-red-500"
+                  }`}
+                >
+                  <div className="pb-3 text-white">
+                    <label htmlFor="message">How can we help?</label>
                   </div>
-                  <div
-                    className={`${"mb-6"} ${
-                      formInputValidity.email ? "" : "border-red-500"
-                    }`}
-                  >
-                    <div className="pb-3 text-white">
-                      <label htmlFor="address">Email</label>
-                    </div>
-                    <input
-                      type="email"
-                      placeholder="you@company.com"
-                      id="email"
-                      name="email"
-                      ref={emailInputRef}
-                      className={`${"w-full rounded-lg p-3 text-gray-50 bg-transparent border-2 border-grey-200 outline-none focus-visible:shadow-none focus:border-blue-200"}
-                      ${!formInputValidity.email && "border-red-500"}`}
-                      required
-                    />
-                  </div>
-
-                  <div
-                    className={`${"mb-6"} ${
-                      formInputValidity.message ? "" : "border-red-500"
-                    }`}
-                  >
-                    <div className="pb-3 text-white">
-                      <label htmlFor="message">How can we help?</label>
-                    </div>
-                    <textarea
-                      rows="6"
-                      placeholder="Tell us a little about your project..."
-                      id="message"
-                      name="message"
-                      className={`${"w-full resize-none rounded-lg p-3 text-gray-50 bg-transparent border-2 border-grey-200 outline-none focus-visible:shadow-none focus:border-blue-200"}
+                  <textarea
+                    rows="6"
+                    placeholder="Tell us a little about your project..."
+                    id="message"
+                    name="message"
+                    className={`${"w-full resize-none rounded-lg p-3 text-gray-50 bg-transparent border-2 border-grey-200 outline-none focus-visible:shadow-none focus:border-blue-200"}
                       ${!formInputValidity.message && "border-red-500"}`}
-                      ref={messageInputRef}
-                    ></textarea>
-                  </div>
-                  <div>
-                    <button
-                      type="submit"
-                      className="w-full text-black hover:text-white bg-white rounded-lg border border-white p-3 transition ease-in-out duration-200 hover:bg-transparent"
-                    >
-                      Send Message
-                    </button>
-                  </div>
-                </form>
-              </div>
+                    ref={messageInputRef}
+                  ></textarea>
+                </div>
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full text-black hover:text-white bg-white rounded-lg border border-white p-3 transition ease-in-out duration-200 hover:bg-transparent"
+                  >
+                    Send Message
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
