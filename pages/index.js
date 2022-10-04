15206612197272f9bce8cbff6f849773e452957f
Aman Sharma <aman@getepiphany.com>
2022-10-04 20:28:26 +0530

    fix: Hide clients

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <Background>
       <Hero />
         <Card />
-        <Client />
+        {/* <Client /> */}
         <Form />
       {/* <div className="">
       </div> */}
