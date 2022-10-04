aec76b7c5ae8c9ee93d576b3272ec2bebf274a00
Aman Sharma <aman@getepiphany.com>
2022-10-04 20:27:25 +0530

    feat: [WIP]Add CTA Section

---
+++ components/sections/GrailCTASection.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+function GrailCTASection() {
+  return (
+    <Fragment>
+      <section className="my-60 lg:my-80">
+        <div className="relative">
+            
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default GrailCTASection;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import ClientSection from "../components/sections/ClientSection";
 import ContactFormSection from "../components/sections/ContactFormSection";
 import HeroSection from "../components/sections/HeroSection";
 import Background from "../components/Background";
+import GrailCTASection from "../components/sections/GrailCTASection";
 
 export default function Home() {
   return (
     <Background>
       <HeroSection />
+      <GrailCTASection />
       <ServicesSection />
       <ClientSection />
       <ContactFormSection />
