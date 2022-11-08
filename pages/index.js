acc4f0f0f7d6253a5d7b4708d39dfd4c84a9e1dc
Manish <manishsdlp222@gmail.com>
2022-11-08 10:44:41 +0530

    feat: add footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Card from "../components/Card";
 import Client from "../components/Client";
+import Footer from "../components/Footer";
 import Form from "../components/Form";
 import Hero from "../components/Hero";
 import Background from "../components/ui/Background";
@@ -8,9 +9,10 @@ export default function Home() {
   return (
     <Background>
       <Hero />
-        <Card />
-        <Client />
-        <Form />
+      <Card />
+      <Client />
+      <Form />
+      <Footer />
       {/* <div className="">
       </div> */}
     </Background>
